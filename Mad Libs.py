230ee15a582060af63b403173697b8f4ac2ad79a
Adhyan03 <77205889+Adhyan03@users.noreply.github.com>
2021-07-05 13:05:38 +0530

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,16 @@
+#Mad Libs
+print('About me Mad Lib')
+food = input(' Silly Word: ')
+job = input('Profession/Job: ')
+Adjective = input('Adjective: ')
+Phrase = input('Phrase/Lyrics/Saying: ')
+Animal = input('Animal: ')
+Verb = input('Verb: ')
+Place = input('Place: ')
+Celebrity = input('Celebrity: ')
+Buy = input('Something you would buy: ')
+Things = input('Things (plural): ')
+
+print('Hi my name is',Celebrity,',but my friends call me',(Adjective +' '+food+'.'),'My favorite color is the color of '
+,Things,'and my favorite thing to do is',Verb,'.My parents were a',Animal,'and',job,', which is why we lived in',Place
+,'.You probably know me from my TV commercial for',Buy,'.I am the one who says,',Phrase,'at the very end!'      )
